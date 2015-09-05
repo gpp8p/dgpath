789770df05aea660a2819172e57d5813568436b9
George Pipkin <gpp8p@Virginia.EDU>
2015-09-05 14:09:00 -0400

    dgpath - default connection code now working

--- server_mysqli/createDefaultConnection.php
+++ server_mysqli/createDefaultConnection.php
@@ -13,6 +13,7 @@ require_once '../server_mysqli/constants.php';
 require_once '../server_mysqli/insertDocument.php';
 require_once '../server_mysqli/loadComponents.php';
 require_once '../server_mysqli/dbparams.php';
+require_once '../server_mysqli/preparedQuery.php';
 
 
 $startId = $_POST['startId'];
@@ -31,10 +32,10 @@ if ($stmt = mysqli_prepare($link, $query)) {
     header('HTTP/1.0 400 bad query - connection insert');
     exit;
 }
-
+$componentId = $startId;
 $connectionId = $stmt->insert_id;
 
-$component_query = "SELECT type from dgpath_component where id = ?";
+$componentQuery = "SELECT type from dgpath_component where id = ?";
 $componentParams = array($componentId);
 $queryResult = mysqli_prepared_query($link,$componentQuery,"s",$componentParams);
 if ($queryResult[0]=="error") {
@@ -56,18 +57,18 @@ foreach($queryResult as $componentData){
 
     }
 }
-$events_query = "SELECT id from dgpath_events where component_id = ? and event_type = ?";
+$eventsQuery = "SELECT id from dgpath_events where component_id = ? and event_type = ?";
 $componentParams = array($componentId, $correctAnswerEventType);
-$queryResult = mysqli_prepared_query($link,$componentQuery,"s",$componentParams);
+$queryResult = mysqli_prepared_query($link,$eventsQuery,"ss",$componentParams);
 if ($queryResult[0]=="error") {
     header('HTTP/1.0 400 connection error in getEventsForSubContext');
     exit;
 }
 foreach($queryResult as $eventData){
-    $thisEventId = $componentData['id'];
+    $thisEventId = $eventData['id'];
     $thisActivateValue = 6;
-    $ruleInsertQuery = "INSERT in dgpath_rules (event_id, connection_id, activate) values (?,?,?)";
-    if ($stmt = mysqli_prepare($link, $query)) {
+    $ruleInsertQuery = "INSERT into dgpath_rules (event_id, connection_id, activate) values (?,?,?)";
+    if ($stmt = mysqli_prepare($link, $ruleInsertQuery)) {
         mysqli_stmt_bind_param($stmt, "sss", $thisEventId, $connectionId, $thisActivateValue);
         mysqli_stmt_execute($stmt);
         if(mysqli_stmt_affected_rows($stmt)==0){
