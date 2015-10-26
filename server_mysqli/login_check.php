22fd5df0061e2217d64dcff7f509ff710f105255
George Pipkin <gpp8p@Virginia.EDU>
2015-10-26 17:34:48 -0400

    dgpath - new login transaction - inserts traversal and user_events records

--- server_mysqli/login_check.php
+++ server_mysqli/login_check.php
@@ -6,13 +6,15 @@
  * Time: 9:08 PM
  */
 session_start();
-require_once '../server/jsonED.php';
-require_once '../server/dbparams_mysqli.php';
-
+require_once '../server_mysqli/constants.php';
+require_once '../server_mysqli/jsonED.php';
+require_once '../server_mysqli/dbparams.php';
+require_once '../server_mysqli/preparedQuery.php';
+require_once '../server_mysqli/evtypes.php';
 
 $username = $_POST['username'];
 $password = $_POST['password'];
-
+$thisSessionId = session_id();
 //$link = mysqli_connect("127.0.0.1", "gpp8p", "kal1ca7", "dgpath");
 
 if (mysqli_connect_errno()) {
@@ -20,25 +22,40 @@ if (mysqli_connect_errno()) {
     exit();
 }
 $loginSuccess = false;
-if ($stmt = mysqli_prepare($link, 'SELECT id FROM dgpath_user where
-                      user_eid=? and password=?')) {
-    /* bind parameters for markers */
-    /* Assumes userid is integer and category is string */
-    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
-    /* execute query */
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result($stmt, $id);
 
-    while (mysqli_stmt_fetch($stmt)) {
-        $loginSuccess=true;
-    }
+$loginQuery = "SELECT id FROM dgpath_user where user_eid=? and password=?";
+$loginEventTraversalQuery = "INSERT session, user_id into dgpath_agent_traversal values (?,?)";
+$loginParams = array($username, $password);
+$queryResult = mysqli_prepared_query($link,$query,"ss",$loginParams);
+$loginSuccess = false;
+$logSuccess = false;
+$traversalEntrySuccess=false;
+foreach($queryResult as $row){
+    $loginSuccess=true;
+    $thisUserId = $row['id'];
 }
-mysqli_stmt_close($stmt);
-mysqli_close($link);
 if($loginSuccess){
     $_SESSION['username'] = $username;
+    if ($stmt = mysqli_prepare($link, $loginEventTraversalQuery)) {
+        mysqli_stmt_bind_param($stmt, "ss",$thisSessionId, $thisUserId );
+        mysqli_stmt_execute($stmt);
+        if(mysqli_affected_rows($link)==0){
+            header('HTTP/1.0 400 Nothing saved - agent_traversal insert');
+            exit;
+        }else {
+            $traversalId = $stmt->insert_id;
+        }
+
+    }
+    $logSuccess = recordLogin($traversalId, $thisUserId);
+
     echo('1');
 }else{
     header('HTTP/1.0 400 Login Failed');
     exit;
 }
+
+
+
+
+
--- server_mysqli/recordUserEvents.php
+++ server_mysqli/recordUserEvents.php
@@ -6,6 +6,13 @@
  * Time: 4:11 PM
  */
 
+require_once '../server_mysqli/constants.php';
+require_once '../server_mysqli/jsonED.php';
+require_once '../server_mysqli/dbparams.php';
+require_once '../server_mysqli/preparedQuery.php';
+require_once '../server_mysqli/loadContent.php';
+require_once '../server_mysqli/evtypes.php';
+
 function recordThisUserEvent($thisEvent, $thisSessionId){
 
     GLOBAL $documentViewed,$documentLinkClicked,$mcViewed,$mcCorrect,$mcAnswerX,$tfTrueSelected,$tfFalseSelected;
@@ -19,4 +26,11 @@ function recordSuccessfulScreenTransfer($thisSessionId, $nextComponentId){
 
     $transferRecordId=null;
     return $transferRecordId;
+}
+
+function recordLogin($thisSessionId, $thisUserId){
+
+    $loginEventTraversalQuery = "INSERT session, user_id into dgpath_agent_traversal values (?,?)";
+
+
 }
