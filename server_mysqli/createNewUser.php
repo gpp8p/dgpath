ebe7e774c2d01853e46392013fb739dda38791ed
George Pipkin <gpp8p@Virginia.EDU>
2015-11-14 22:07:11 -0500

    dgpath - create new user queries

--- server_mysqli/createNewUser.php
+++ server_mysqli/createNewUser.php
@@ -24,10 +24,16 @@ $eid = $_POST['eid'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $userType = $_POST['userType'];
+$projectId = $_POST['projectId'];
 
 $userEidQuery = "SELECT id, user_eid, password, last_name, first_name, email from dgpath_user where user_eid = ?";
+
 $userInsertQuery = "INSERT into dgpath_user (user_eid, password, last_name, first_name, email) values (?,?,?,?,?)";
+
 $groupInsertForUserQuery = "INSERT into dgpath_group (label) values (?)";
+
+$userInGroupQuery = "INSERT into dgpath_user_in_group (user_id, group_id) values (?,?)";
+
 $isUserInCourseQuery = "select dgpath_group.id, dgpath_group.label from dgpath_user, dgpath_user_in_group, dgpath_group, dgpath_cando_project, dgpath_permission where ";
 $isUserInCourseQuery = $isUserInCourseQuery."dgpath_group.id = dgpath_user_in_group.group_id ";
 $isUserInCourseQuery = $isUserInCourseQuery."and dgpath_user_in_group.user_id = dgpath_user.id ";
@@ -37,9 +43,85 @@ $isUserInCourseQuery = $isUserInCourseQuery."and dgpath_cando_project.project_id
 $isUserInCourseQuery = $isUserInCourseQuery."and dgpath_cando_project.permission_id = dgpath_permission.id ";
 $isUserInCourseQuery = $isUserInCourseQuery."and dgpath_permission.label = ?";
 
+$userInGroupInsertQuery = "INSERT into dgpath_user_in_group (user_id, group_id) values (?,?)";
+
+$candoProjectInsertQuery = "INSERT into dgpath_cando_project (project_id, group_id, permission_id) values (?,?,?)";
+
+$existingEid = false;
+
+$queryParams = array($eid);
+$queryResult = mysqli_prepared_query($link,$userEidQuery,"s",$queryParams);
+$eidFound = false;
+foreach($queryResult as $thisQueryResult){
+    $eidFound=true;
+    $thisEidId = $thisQueryResult['id'];
+    $thisFirstName = $thisQueryResult['first_name'];
+    $thisLastName = $thisQueryResult['last_name'];
+    $thisEmail = $thisQueryResult['email'];
+}
+
+if($eidFound){
+    $verifiedUser=false;
+    if((strcasecmp($thisFirstName, $firstName)==0)&&(strcasecmp($thisLastName, $lastName)==0)){
+        $verifiedUser=true;
+    }
+    if(!$verifiedUser){
+        $returnValues = array("msg"=>"duplicateUserId", "firstName"=>$thisFirstName, "lastName"=>$thisLastName, "eid"=>$thisEid );
+        $returnDataJson = json_encode($returnValues);
+        echo($returnDataJson);
+        exit;
+    }else{
+        $existingEid = true;
+    }
+}
+
+if(!$existingEid){
+    if ($stmt = mysqli_prepare($link, $userInsertQuery)) {
+        mysqli_stmt_bind_param($stmt, "sssss", $eid, $password, $lastName, $firstName, $email);
+        mysqli_stmt_execute($stmt);
+        if(mysqli_stmt_affected_rows($stmt)<=0){
+            header('HTTP/1.0 400 Nothing saved - user insert');
+            exit;
+        }else{
+            $thisNewUserId = $stmt->insert_id;
+        }
+    }else{
+        header('HTTP/1.0 400 bad query - collection insert');
+        exit;
+    }
+    if ($stmt = mysqli_prepare($link, $groupInsertForUserQuery)) {
+        $newUserGroupLabel = $eid." group";
+        mysqli_stmt_bind_param($stmt, "s", $newUserGroupLabel);
+        mysqli_stmt_execute($stmt);
+        if(mysqli_stmt_affected_rows($stmt)<=0){
+            header('HTTP/1.0 400 Nothing saved - group insert');
+            exit;
+        }else{
+            $thisNewUserGroupId = $stmt->insert_id;
+        }
+    }else{
+        header('HTTP/1.0 400 bad query - collection insert');
+        exit;
+    }
+    if ($stmt = mysqli_prepare($link, $userInGroupQuery)) {
+        mysqli_stmt_bind_param($stmt, "ii", $thisNewUserId, $thisNewUserGroupId);
+        mysqli_stmt_execute($stmt);
+        if(mysqli_stmt_affected_rows($stmt)<=0){
+            header('HTTP/1.0 400 Nothing saved - user in group insert');
+            exit;
+        }else{
+            $thisNewUserInGroupId = $stmt->insert_id;
+        }
+    }else{
+        header('HTTP/1.0 400 bad query - collection insert');
+        exit;
+    }
+}
 
 
 
 
-
-echo('ok');
+$returnValues = array("msg"=>"userCreated", "firstName"=>$firstName, "lastName"=>$lastName, "eid"=>$eid );
+$returnDataJson = json_encode($returnValues);
+echo($returnDataJson);
+exit;
